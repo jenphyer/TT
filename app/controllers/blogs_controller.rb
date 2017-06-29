7bd4ab83cddebb4ba0cbe0f2dbbf503dc456b308
jenhyer <jenphyer-shan0628@hotmail.com>
2017-06-29 23:13:12 +0800

    Backend done

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,50 @@
+class BlogsController < ApplicationController
+  http_basic_authenticate_with name: "misspanda", password: "secret", except: [:index, :show]
+
+  def index
+    @blogs = Blog.all
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+
+    if @blog.save
+      redirect_to blogs_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+
+    if @blog.update(blog_params)
+      redirect_to blogs_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+    redirect_to blogs_path
+  end
+
+  private
+    def blog_params
+      params.require(:blog).permit(:title, :content)
+    end
+end
